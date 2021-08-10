0cbed8ce43df407dabb8ea031898f3dcbfc52fb6
zhao85222686 <82257788+zhao85222686@users.noreply.github.com>
2021-08-10 20:42:55 +0800

    结构体

---
+++ 每日练习/struct.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct Date
+{
+	int year, month, day;
+};
+
+struct Person
+{
+	Date Dt;
+	char Name[20];
+	int age;
+};
+
+int main()
+{
+	Person s1 = { {1987,11,18},"张三",10 };
+	Person s2 = { {1986,9,18},"李四",11 };
+	Person s3 = { {1985,10,18},"王五",9 };
+	cout << "姓名：" << s1.Name << "，年龄：" << s1.age << ",出生日期:"
+		<< s1.Dt.year << "," << s1.Dt.month << "," << s1.Dt.day << endl;
+	cout << "姓名：" << s2.Name << "，年龄：" << s2.age << ",出生日期:"
+		<< s2.Dt.year << "," << s2.Dt.month << "," << s2.Dt.day << endl;
+	cout << "姓名：" << s3.Name << "，年龄：" << s3.age << ",出生日期:"
+		<< s3.Dt.year << "," << s3.Dt.month << "," << s3.Dt.day << endl;
+
+	return 0;
+}
