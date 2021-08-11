05f02e05fb43602c9c38dfe0681581f6924bd6cf
zhao85222686 <82257788+zhao85222686@users.noreply.github.com>
2021-08-11 13:38:07 +0800

    书上的例子

--- 每日练习/struct.cpp
+++ 每日练习/struct.cpp
@@ -28,3 +28,43 @@ int main()
 
 	return 0;
 }
+
+***********************************************************************************************************
+#include <iostream>
+
+using namespace std;
+
+struct Vector
+{
+	int sz;
+	double* elem;
+};
+
+void vector_init(Vector& v, int s)
+{
+	v.elem = new double[s];
+	v.sz = s;
+}
+
+double read_and_sum(int s)
+{
+	Vector v;
+	vector_init(v, s);
+	for (int i=0;i<s;i++)
+	{
+		cin >> v.elem[i];
+	}
+	double sum = 0;
+	for (int i=0;i<s;i++)
+	{
+		sum += v.elem[i];
+	}
+	return sum;
+}
+
+int main()
+{
+	
+
+	return 0;
+}
